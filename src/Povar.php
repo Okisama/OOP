4b798a041ab4054f66de092adafe68d38f907236
= <=>
2016-10-31 02:34:11 +0300

    Add povar.php

--- src/Povar.php
+++ src/Povar.php
@@ -5,3 +5,34 @@
  * Date: 30.10.16
  * Time: 16:43
  */
+
+namespace Vendor\Reporter;
+
+
+use Vendor\Reporter\Havchik\FoodInterface;
+use Vendor\Reporter\Gotovka\CookInterface;
+
+
+class Povar
+{
+    /**
+     * @var FoodInterface
+     */
+    private $food;
+    /**
+     * @param Foodinterface $food
+     */
+    public function __construct(FoodInterface $food)
+    {
+        $this->food = $food;
+    }
+    /**
+     * @param string $url
+     * @param CookInterface $cook
+     */
+    public function cookFood($url, CookInterface $cook)
+    {
+        $recipe = $this->food->takeSome($url);
+        $cook->cook($recipe);
+    }
+}
