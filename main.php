0ce39c353b0a3fc90d14b701481e7a9a64d69d22
= <=>
2016-10-31 02:51:11 +0300

    Add main.php

--- main.php
+++ main.php
@@ -6,3 +6,9 @@
  * Time: 16:44
  */
 
+require_once __DIR__ . '/vendor/autoload.php';
+
+$havchik = new \Vendor\Reporter\Havchik\Kartofan('path_to_recipe');
+$povar = new \Vendor\Reporter\Povar($havchik);
+$povar->cookFood('http://example.com', new \Vendor\Reporter\Gotovka\FryCook(__DIR__ . 'fryPotato'));
+$povar->cookFood('http://example.com', new \Vendor\Reporter\Gotovka\FryCookExtendedViaInheritance(__DIR__ . 'extraFryPotato'));
